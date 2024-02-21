282b5d9504a697c62bcfac5d7b6c5d875d06df57
Himanshi Sharma <himanshish456@gmail.com>
2024-02-21 23:57:21 +0530

    footer responsiveness

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import classes from "./footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.wrapper}>
         <div className={classes.col}>
-          <h2>About the app</h2>
+          <h2>About the Web</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-            sapiente minima in, aperiam ratione laboriosam quasi perferendis
-            harum, voluptas, voluptate pariatur illum accusantium. Maiores
-            perspiciatis distinctio quos, impedit eveniet repudiandae.
+            Designed and Improved a website specifically tailored for showcasing
+            creative blogs from various users. The functionalities encompasses
+            2-4 features such as update and delete blog, like and comment the
+            blog.
           </p>
         </div>
         <div className={classes.col}>
-          <h2>Contants</h2>
-          <span>Phone +123456</span>
-          <span>Github</span>
-          <span>Linkedin</span>
+          <h2>Contacts</h2>
+          <Link href="https://github.com/himanshi-sharma-123">Github</Link>
+          <Link href="https://www.linkedin.com/in/himanshi-sharma-564548202/">
+            Linkedin
+          </Link>
         </div>
         <div className={classes.col}>
           <h2>Location</h2>
